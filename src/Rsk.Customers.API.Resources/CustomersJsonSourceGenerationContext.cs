b990e931cd03c8cf2f024daf05145dced52c6294
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-10-04 15:26:40 +0100

    Add json serialization using source generators to the API.

---
+++ src/Rsk.Customers.API.Resources/CustomersJsonSourceGenerationContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Rsk.Customers.API.Resources;
+
+[JsonSerializable(typeof(Customer))]
+public partial class CustomersJsonSourceGenerationContext : JsonSerializerContext
+{
+}
--- src/Rsk.Customers.API/Program.cs
+++ src/Rsk.Customers.API/Program.cs
@@ -1,7 +1,9 @@
+using Rsk.Customers.API.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>options.JsonSerializerOptions.AddContext<CustomersJsonSourceGenerationContext>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
