3b17123e880be3851e7d6c62c613ee611da83ea2
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-10-16 08:43:06 +0100

    Use a shared project and update NuGet packages.

--- src/Rsk.Customers.API.Client/Program.cs
+++ src/Rsk.Customers.API.Client/Program.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Refit;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Rsk.Customers.API.Resources;
 
 var serviceCollection =new ServiceCollection();
-serviceCollection.AddRefitClient<ICustomersAPI>()
+var options = new JsonSerializerOptions();
+options.AddContext<CustomersJsonSourceGenerationContext>();
+serviceCollection.AddRefitClient<ICustomersAPI>(new RefitSettings
+{
+        ContentSerializer = new SystemTextJsonContentSerializer(options)
+})
     .ConfigureHttpClient(c =>c.BaseAddress =new Uri("http://localhost:5111"));
+    
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
 var customersAPI =serviceProvider.GetRequiredService<ICustomersAPI>();
--- src/Rsk.Customers.API.Resources/CustomersJsonSourceGenerationContext.cs
+++ src/Rsk.Customers.API.Resources/CustomersJsonSourceGenerationContext.cs
@@ -1,7 +1,9 @@
 ﻿using System.Text.Json.Serialization;
 
 namespace Rsk.Customers.API.Resources;
-
+#if (!IsClient)
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
+#endif
 [JsonSerializable(typeof(Customer))]
 public partial class CustomersJsonSourceGenerationContext : JsonSerializerContext
 {
