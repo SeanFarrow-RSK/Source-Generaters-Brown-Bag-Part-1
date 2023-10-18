ae1bda0b16f42ab4ab46e062f1ff04278a79fa7a
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-10-18 08:12:05 +0100

    Add console logging

--- src/Rsk.Customers.API.Client/Program.cs
+++ src/Rsk.Customers.API.Client/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var serviceCollection =new ServiceCollection();
 serviceCollection.AddHttpClient<ICustomerService, CustomerService>();
 var serviceProvider = serviceCollection.BuildServiceProvider();
 var customerService =serviceProvider.GetRequiredService<ICustomerService>();
 var customer =await customerService.GetCustomer(1);
-Console.ReadKey();
+if (customer is null)
+{
+    Console.WriteLine("No customer found.");
+}
+else
+{
+        Console.WriteLine($"A customer with id {customer.Id} has been found.");
+}
